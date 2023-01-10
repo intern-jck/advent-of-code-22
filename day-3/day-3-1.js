de6c9d9225562c898cb020fac5150f67bb37d09b
intern-laptop <justin.c.kirk@gmail.com>
2023-01-09 22:09:36 -0500

    adds comments to day 3

--- day-3/day-3-1.js
+++ day-3/day-3-1.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 const data = fs.readFileSync('input.txt', 'UTF-8').split('\n');
 
-let allEven = true;
 let prioritySum = 0;
 
 // const priorityValues = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
+// Test to see the char code values
 console.log('A'.charCodeAt(0))
 console.log('Z'.charCodeAt(0))
 console.log('a'.charCodeAt(0))
 console.log('z'.charCodeAt(0))
-
 
 while (data.length) {
     const item = data.shift();
@@ -18,15 +17,16 @@ while (data.length) {
     // compartments 1 and 2
     const comp1 = item.substring(0, item.length / 2);
     const comp2 = item.substring(item.length / 2);
-    // console.log(comp1)
-    // console.log(comp2)
 
-    // for each item type
+    // for each item type,
     for (let i in comp1) {
+        // if its in comp2,
         if (comp2.includes(comp1[i])) {
-            console.log('found:', comp1[i], comp1.charCodeAt(i))
+            // get the item value,
             const itemValue = comp1.charCodeAt(i);
+            // scale it according to prioritory ordering,
             if (itemValue >= 97 && itemValue <= 122) {
+                // then add to sum.
                 prioritySum += itemValue - 96;
             } else if (itemValue >= 65 && itemValue <= 90) {
                 prioritySum += itemValue - 64 + 26;
@@ -36,4 +36,5 @@ while (data.length) {
     }
 }
 
+// show sum at end.
 console.log('sum', prioritySum)
