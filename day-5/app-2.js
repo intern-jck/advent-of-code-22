c825d82cd955a38eb2cfb1da8f3128dd5684eada
intern laptop <justin.c.kirk@gmail.com>
2023-01-24 23:44:42 -0500

    day 5 part 2 complete

--- day-5/app-2.js
+++ day-5/app-2.js
@@ -33,9 +33,6 @@ for (let i in inputData) {
     }
 }
 
-// console.log(data)
-// return;
-
 // get data to rotate
 const dataToRotate = [];
 for (let i in data) {
@@ -46,76 +43,40 @@ for (let i in data) {
     dataToRotate.push(newData)
 }
 
-// console.table(dataToRotate)
-// return;
-// console.table(dataToRotate)
-
 // Rotate data set to build data stacks
 
-const dataStacksObj = {};
 const dataStacksArr = [[], [], [], [], [], [], [], [], [], []];
-
 const stackNums = dataToRotate[dataToRotate.length - 1];
-
 for (let i = dataToRotate.length - 2; i >= 0; i--) {
     const dataRow = dataToRotate[i];
-    // console.log(dataRow)
-
-    // Store data as an object
-    // for (let j in dataRow) {
-    //     const stackNum = stackNums[j];
-
-    //     if (dataStacksObj[stackNum] && dataRow[j]) {
-    //         dataStacksObj[stackNum].push(dataRow[j]);
-    //     } else if (dataRow[j]) {
-    //         dataStacksObj[stackNum] = [dataRow[j]]
-    //     }
-    // }
-
-
-
     for (let j in dataRow) {
-
         const stackNum = stackNums[j];
-        // console.log('stackNum:', stackNums[j])
-        // return;
-        // console.log(dataRow[i], stackNum)
         if (dataRow[j]) {
-            // console.log('data row:', j, data[j])
             dataStacksArr[stackNum].push(dataRow[j])
         }
-
     }
-    // console.log('\n')
-
 }
-
-// Verify data and moves
-// console.table(dataStacksArr)
-// return;
-// console.table(dataStacksArr)s
-// console.log(moves)
-
 
 // Now loop through moves and adjust data
 for (let i in moves) {
-    // console.log(moves[i])
     let counter = 0;
+    const moveStack = [];
     while (counter < moves[i].amount) {
         const elementToMove = dataStacksArr[moves[i].from].pop();
-        // console.log('moving', elementToMove)
-        dataStacksArr[moves[i].to].push(elementToMove);
+        // dataStacksArr[moves[i].to].push(elementToMove);
+        moveStack.push(elementToMove);
         counter += 1;
     }
+    while (moveStack.length) {
+        dataStacksArr[moves[i].to].push(moveStack.pop())
+    }
+    // dataStacksArr[moves[i].to].push(moveStack);
 }
-
 console.table(dataStacksArr)
 
 // Get the top element from each stack
-
 const stacksCount = dataStacksArr.length - 1;
 let crates = '';
-
 for (let i = 1; i <= stacksCount; i++) {
     const stackCount = dataStacksArr[i].length - 1;
     const topOfStack = dataStacksArr[i][stackCount]
