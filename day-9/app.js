18d1b9ec8cc5efdece817b4a739e4bec7ec0d176
intern laptop <justin.c.kirk@gmail.com>
2023-02-13 21:34:24 -0500

    day 9 mvp

---
+++ day-9/app.js
@@ -0,0 +1,104 @@
+const fs = require('fs');
+const inputData = fs.readFileSync('test.txt', 'utf-8').split('\n');
+
+class Knot {
+
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.path = [];
+        this.pathLength = 0;
+        this.visited = new Set();
+    };
+
+    getCurrentPosition() {
+        const pos = this.path.slice().pop();
+        if (pos) {
+            return pos;
+        }
+        return [this.x, this.y];
+    };
+
+    getPreviousPosition() {
+        const pos = this.path.slice()[this.path.length - 2];
+        return pos;
+    };
+
+    getPath() {
+        return this.path.slice();
+    };
+
+    move(dir) {
+        switch (dir) {
+            case 'L':
+                this.x--;
+                break;
+            case 'R':
+                this.x++;
+                break;
+            case 'U':
+                this.y++;
+                break;
+            case 'D':
+                this.y--;
+                break;
+        }
+
+        this.path.push([this.x, this.y]);
+    };
+
+    moveTo(x, y) {
+        this.x = x;
+        this.y = y;
+        this.path.push([this.x, this.y]);
+    };
+
+    getDistance(knot) {
+        const xDistance = (knot.x - this.x);
+        const yDistance = (knot.y - this.y);
+        const distance = Math.sqrt(xDistance ** 2 + yDistance ** 2);
+        return distance;
+    };
+
+
+
+};
+
+const head = new Knot();
+const tail = new Knot();
+
+const maxDistance = Math.sqrt(2);
+
+// Move knots based on input
+for (let i = 0; i < inputData.length; i++) {
+
+    // Parse each line into a direction and number of steps.
+    const input = inputData[i].split(' ');
+    const direction = input[0];
+    const steps = input[1];
+
+    // Move head one step for each number of steps
+    for (let i = 0; i < steps; i++) {
+
+        head.move(direction);
+
+        const headCurrent = head.getCurrentPosition();
+        const headPrevious = head.getPreviousPosition();
+        const headDistance = head.getDistance(tail);
+
+        if (headDistance > maxDistance) {
+            tail.moveTo(headPrevious[0], headPrevious[1])
+        }
+
+        const tailCurrent = tail.getCurrentPosition();
+        console.log(headCurrent, tailCurrent);
+
+    }
+
+}
+
+// console.table(tail.getPath());
+// console.table(head.getPath());
+
+console.log(tail.getPath().length, head.getPath().length);
+
---
+++ day-9/scratch.js
@@ -0,0 +1,14 @@
+
+// map the pos list
+function traceRoute(route) {
+
+    let field = [];
+
+    for (let i in route) {
+        let _pos = route[i];
+        console.log(_pos)
+    }
+
+}
+
+traceRoute(headPosList);
